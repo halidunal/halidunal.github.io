58ff4ea0bab788111a72ed350abf98338ba660aa
halidunal <halidunal@gmail.com>
2021-03-18 21:09:24 +0300

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Card from "../components/Card"
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Card/>
+            </div>
+        )
+    }
+}
